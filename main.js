2e6878d763c4a8ebe684b88922df8bed4b0f3570
Akhil Aravindakshan <akhil.dev.00782@gmail.com>
2024-01-11 03:39:38 -0400

    add scene and camera to the renderer

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ const renderer = new THREE.WebGLRenderer(); // Renderer
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x595959, 1);
 document.body.prepend(renderer.domElement);
+
+// Camera
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+camera.position.set(-30, 30);
+
+renderer.render(scene, camera);
